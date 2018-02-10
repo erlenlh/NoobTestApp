bd513f143d84cbb8f933c9236ee6335cb7fdefba
Erlenlh <erlend.helland@gmail.com>
2018-02-10 18:22:11 +0100

    API added for users and groups

--- testApp/quickstart/views.py
+++ testApp/quickstart/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from testApp.quickstart.serializers import GroupSerializer, UserSerializer
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    # API Endpoint to view and edit users
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+class GroupViewSet(viewsets.ModelViewSet):
+    # API endpoint for groups
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
